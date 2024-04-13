44fea9c73f0bc9da18d37a96eee2026b4c183a9e
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-04-13 15:00:58 +0100

    my-portofolio using React/PHP/SQL

---
+++ src/components/organism/Navigation.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          {" "}
+          <a href="#home"></a> Home{" "}
+        </li>
+        <li>
+          {" "}
+          <a href="#about"></a> Abiyt{" "}
+        </li>
+        <li>
+          {" "}
+          <a href="#projects"></a> Projects{" "}
+        </li>
+        <li>
+          {" "}
+          <a href="#contact"></a> Contact{" "}
+        </li>
+      </ul>
+    </nav>
+  );
+}
