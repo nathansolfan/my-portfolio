90df09ff2703f9c990f9bc7c802801dfde08a77a
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-04-22 21:13:08 +0100

    Update CSS styles for Booking and Header components

--- src/components/organism/Header.js
+++ src/components/organism/Header.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../Styles/Header.css";
+import nathan from "../../images/nathan1.webp";
 
 export default function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header className="header">
+    <header className={`header ${scrolled ? "scrolled" : "transparent"}`}>
       <nav className="navbar">
+        <img src={nathan} alt="" />
         <ul className="nav-list">
           <li className="nav-item">
             <Link to="/" className="nav-link">
