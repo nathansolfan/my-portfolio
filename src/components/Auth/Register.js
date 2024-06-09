01691a5ad069383c9b058b6d7f7e346639014c0d
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-06-09 23:33:50 +0100

    refactor: Update Register component to use React and axios for user registration

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios";
+import "../Styles/Register.css"; // Import your CSS file for styling
 
 export default function Register() {
   return <div>Register</div>;
