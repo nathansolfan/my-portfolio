22e3cae70d5e353a16e8082bd8543bcd48a7dd90
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-06-26 23:39:29 +0100

    refactor: Add email and password state variables to Login component

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,5 +1,9 @@
-import React from "react";
-
+import React, { useState } from "react";
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
   return <div>Login</div>;
 }
